4f584933875d79ccfe559402457759244f048073
Guilherme Bustamante <guilherme_bustamante@live.com>
2024-01-10 14:42:15 -0300

    feat: add initial content to main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,34 @@
+import Image from "next/image";
+import logo from "@/public/logoLight.png";
+import { Button } from "@/components/ui/button";
+import { FiLogIn } from "react-icons/fi";
+import { LoginButton } from "@/components/auth/login-button";
+
 export default function Home() {
   return (
-    <main>
-      <h1>Homepage</h1>
+    <main className="flex h-full flex-col items-center justify-center bg-[url('/bghome.png')] bg-cover bg-center bg-no-repeat">
+      <div className="flex flex-col items-center justify-center space-y-6 text-center bg-neutral-500/50 mx-5 py-5 rounded-3xl">
+        <Image
+          src={logo}
+          alt="AuthHero"
+          sizes="100vw"
+          height={0}
+          width={0}
+          className="h-auto w-40"
+        />
+        <p className="mx-auto w-full px-6 font-light md:max-w-xl text-white">
+          Sua solução completa de autenticação, projetada para proporcionar
+          segurança de ponta a ponta.
+        </p>
+
+        <div className="w-full px-5">
+          <LoginButton>
+            <Button size={"lg"} variant={"default"} className="w-full">
+              Login <FiLogIn className="ml-2" size={20} />
+            </Button>
+          </LoginButton>
+        </div>
+      </div>
     </main>
-  )
+  );
 }
