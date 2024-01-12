dd95a9c7daa0ef60ca78a97df7c8f2b55978244d
Guilherme Bustamante <guilherme_bustamante@live.com>
2024-01-12 15:54:24 -0300

    feat: add auth configs and middleware to protect routes

---
+++ routes.ts
@@ -0,0 +1,26 @@
+/**
+ * Array de rotas publicas
+ * Não necessita que o usuário esteja logado
+ * @type {string[]}
+ */
+export const publicRoutes = ["/"];
+
+/**
+ * Array de rotas usadas para autentificação
+ * Essas rotas irão redirecionar os usuários logados para /settings
+ * @type {string[]}
+ */
+export const authRoutes = ["/auth/login", "/auth/register"];
+
+/**
+ * Prefixo para as rotas de autentificação
+ * Rotas que possuem esse prefixo são usadas para autentificação com API(NextAuth)
+ * @type {string}
+ */
+export const apiAuthPrefix = "/api/auth";
+
+/**
+ * Rota padrão de redirecionamento após o login
+ * @type {string}
+ */
+export const DEFAULT_LOGIN_REDIRECT = "/settings"
