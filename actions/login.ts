deae49bc053ff2216a28114898712cd48ece4a9b
Guilherme Bustamante <guilherme_bustamante@live.com>
2024-01-12 16:19:09 -0300

    feat: add login function

--- actions/login.ts
+++ actions/login.ts
@@ -1,7 +1,11 @@
 "use server";
 
 import * as z from "zod";
+
+import { signIn } from "@/auth";
 import { LoginSchema } from "@/schemas";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
+import { AuthError } from "next-auth";
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
   const validatedFields = LoginSchema.safeParse(values);
@@ -10,5 +14,24 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
     return { error: "Crendenciais invalidas!" };
   }
 
-  return { success: "Email enviado!" };
+  const { email, password } = validatedFields.data;
+
+  try {
+    await signIn("credentials", {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGIN_REDIRECT,
+    });
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return { error: "Credenciais invalidas!" };
+        default:
+          return { error: "Ocorreu um erro ao fazer login!" };
+      }
+    }
+
+    throw error;
+  }
 };
