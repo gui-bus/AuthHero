94f7b9dc09be7c6af6ad9cad43e52cf87eadfd80
Guilherme Bustamante <guilherme_bustamante@live.com>
2024-01-16 17:04:11 -0300

    feat: extend user to contain istwofactorenabled

--- auth.ts
+++ auth.ts
@@ -60,6 +60,11 @@ export const {
       if (token.role && session.user) {
         session.user.role = token.role as UserRole;
       }
+
+      if (session.user) {
+        session.user.isTwoFactorEnabled = token.isTwoFactorEnabled as boolean;
+      }
+
       return session;
     },
     async jwt({ token }) {
@@ -70,6 +75,7 @@ export const {
       if (!existingUser) return token;
 
       token.role = existingUser.role;
+      token.isTwoFactorEnabled = existingUser.isTwoFactorEnabled;
 
       return token;
     },
